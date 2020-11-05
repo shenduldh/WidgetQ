f81af0aa6ad43f2a97583bce97b3a841c9edba8e
shendu <274319412@qq.com>
2020-11-05 21:15:37 +0800

    change little

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 const WQ = importModule('core.js').WQ
 const wq = new WQ({
     data: {
-        font: new Font('systemFont', 15),
-        color: Color.yellow(),
-        text: '我的第一个 WidegetQ 小组件!',
-        img: img,
-        idiom: idiom.content
     },
     template: {
         small: ``,
